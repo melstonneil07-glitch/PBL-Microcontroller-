c1483d3f55cb85c25f4902d6d3df7bc978884a0d
vaishnavi <thevaishushetty@gmail.com>
2026-09-04 13:27:03 +0530

    Add DataMemory

--- src/memory/DataMemory.java
+++ src/memory/DataMemory.java
@@ -1,1 +1,14 @@
+package memory;
 
+public class DataMemory {
+
+    private int[] memory = new int[256];
+
+    public void write(int address, int value) {
+        memory[address] = value;
+    }
+
+    public int read(int address) {
+        return memory[address];
+    }
+}
