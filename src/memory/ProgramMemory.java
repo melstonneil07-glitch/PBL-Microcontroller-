ab71211d8c97ac333b389b94cb2f81fc3e642d76
vaishnavi <thevaishushetty@gmail.com>
2026-09-04 13:19:51 +0530

    Add ProgramMemory

--- src/memory/ProgramMemory.java
+++ src/memory/ProgramMemory.java
@@ -1,1 +1,14 @@
+package memory;
 
+public class ProgramMemory {
+
+    private String[] memory = new String[256];
+
+    public void write(int address, String instruction) {
+        memory[address] = instruction;
+    }
+
+    public String read(int address) {
+        return memory[address];
+    }
+}
