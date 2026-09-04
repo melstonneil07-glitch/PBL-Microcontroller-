b52299c5aca5a197d8d6d9a9abd730a12badd68e
vaishnavi <thevaishushetty@gmail.com>
2026-09-04 14:23:57 +0530

    Update ProgramMemory for MS51FB9AE

--- src/memory/ProgramMemory.java
+++ src/memory/ProgramMemory.java
@@ -2,13 +2,34 @@ package memory;
 
 public class ProgramMemory {
 
-    private String[] memory = new String[256];
+    // MS51FB9AE has 16 KB program memory
+    private static final int MEMORY_SIZE = 16 * 1024;
 
-    public void write(int address, String instruction) {
-        memory[address] = instruction;
+    private byte[] memory = new byte[MEMORY_SIZE];
+
+    // Write one byte into program memory
+    public void write(int address, byte value) {
+        checkAddress(address);
+        memory[address] = value;
     }
 
-    public String read(int address) {
+    // Read one byte from program memory
+    public byte read(int address) {
+        checkAddress(address);
         return memory[address];
+    }
+
+    // Check whether the address is valid or not
+    private void checkAddress(int address) {
+        if (address < 0 || address >= MEMORY_SIZE) {
+            throw new IllegalArgumentException(
+                "Invalid program memory address: " + address
+            );
+        }
+    }
+
+    // Get program memory size
+    public int getSize() {
+        return MEMORY_SIZE;
     }
 }
