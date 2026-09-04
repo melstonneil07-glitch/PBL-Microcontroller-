dab4f9cf77d2eda62ae8bc9baca8c1195c2ce33c
vaishnavi <thevaishushetty@gmail.com>
2026-09-04 21:27:41 +0530

    Add DataMemory

--- src/memory/DataMemory.java
+++ src/memory/DataMemory.java
@@ -2,13 +2,30 @@ package memory;
 
 public class DataMemory {
 
-    private int[] memory = new int[256];
-
-    public void write(int address, int value) {
+    // MS51FB9AE has 256 bytes of internal RAM
+    private static final int MEMORY_SIZE = 256;
+    private byte[] memory = new byte[MEMORY_SIZE];
+    // Writingone byte into data memory
+    public void write(int address, byte value) {
+        checkAddress(address);
         memory[address] = value;
     }
-
-    public int read(int address) {
+    // Read one byte from data memory
+    public byte read(int address) {
+        checkAddress(address);
         return memory[address];
+    }
+    // Check whether the address is valid or not
+    private void checkAddress(int address) {
+        if (address < 0 || address >= MEMORY_SIZE) {
+            throw new IllegalArgumentException(
+                "Invalid data memory address: " + address
+            );
+        }
+    }
+
+    // Get data memory size
+    public int getSize() {
+        return MEMORY_SIZE;
     }
 }
