9116d1838a8241f7aa91624e0b88b3e7bac831fb
disha-npoojary <dishanpoojary641@gmail.com>
2026-09-06 11:55:36 +0530

    Update ControlPanel.java

--- src/gui/ControlPanel.java
+++ src/gui/ControlPanel.java
@@ -1,1 +1,43 @@
+package gui;
 
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ControlPanel extends JPanel {
+
+    private JButton loadButton;
+    private JButton resetButton;
+    private JButton stepButton;
+    private JButton runButton;
+
+    public ControlPanel() {
+        setLayout(new FlowLayout());
+
+        loadButton = new JButton("Load");
+        resetButton = new JButton("Reset");
+        stepButton = new JButton("Step");
+        runButton = new JButton("Run");
+
+        add(loadButton);
+        add(resetButton);
+        add(stepButton);
+        add(runButton);
+    }
+
+    public JButton getLoadButton() {
+        return loadButton;
+    }
+
+    public JButton getResetButton() {
+        return resetButton;
+    }
+
+    public JButton getStepButton() {
+        return stepButton;
+    }
+
+    public JButton getRunButton() {
+        return runButton;
+    }
+}
