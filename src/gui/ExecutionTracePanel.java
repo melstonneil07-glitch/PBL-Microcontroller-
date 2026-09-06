cc34ac5e2a7b1fe9c5ad681fda3ca9a450fddc00
disha-npoojary <dishanpoojary641@gmail.com>
2026-09-06 11:57:32 +0530

    Update ExecutionTracePanel.java

--- src/gui/ExecutionTracePanel.java
+++ src/gui/ExecutionTracePanel.java
@@ -1,1 +1,33 @@
+package gui;
 
+import java.awt.BorderLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class ExecutionTracePanel extends JPanel {
+
+    private JTextArea traceArea;
+
+    public ExecutionTracePanel() {
+        setLayout(new BorderLayout());
+        setBorder(
+            BorderFactory.createTitledBorder("Execution Trace")
+        );
+
+        traceArea = new JTextArea(10, 60);
+        traceArea.setEditable(false);
+
+        add(new JScrollPane(traceArea), BorderLayout.CENTER);
+    }
+
+    public void clearTrace() {
+        traceArea.setText("");
+    }
+
+    public void addTrace(String text) {
+        traceArea.append(text + "\n");
+        traceArea.setCaretPosition(traceArea.getDocument().getLength());
+    }
+}
