cee9a2299596465b35e55fe20c19469b833271d1
disha-npoojary <dishanpoojary641@gmail.com>
2026-09-07 11:15:27 +0530

    Update ProgramPanel.java

--- src/gui/ProgramPanel.java
+++ src/gui/ProgramPanel.java
@@ -2,6 +2,7 @@ package gui;
 
 import cpu_core.Instruction;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
@@ -12,13 +13,17 @@ import javax.swing.JScrollPane;
 
 public class ProgramPanel extends JPanel {
 
+    private JLabel categoryLabel;
     private DefaultListModel<String> programModel;
     private JList<String> programList;
     private JLabel currentInstructionLabel;
 
     public ProgramPanel() {
+
         setLayout(new BorderLayout());
-        setBorder(BorderFactory.createTitledBorder("Loaded Program"));
+        setBorder(
+            BorderFactory.createTitledBorder("Loaded Program")
+        );
 
         programModel = new DefaultListModel<>();
         programList = new JList<>(programModel);
@@ -27,14 +32,35 @@ public class ProgramPanel extends JPanel {
             "Current Instruction: None"
         );
 
-        add(new JScrollPane(programList), BorderLayout.CENTER);
-        add(currentInstructionLabel, BorderLayout.SOUTH);
+        categoryLabel = new JLabel(
+            "Category: None"
+        );
+
+        add(
+            new JScrollPane(programList),
+            BorderLayout.CENTER
+        );
+
+        JPanel infoPanel = new JPanel(
+            new GridLayout(2, 1)
+        );
+
+        infoPanel.add(currentInstructionLabel);
+        infoPanel.add(categoryLabel);
+
+        add(
+            infoPanel,
+            BorderLayout.SOUTH
+        );
     }
 
-    public void showProgram(ArrayList<Instruction> program) {
+    public void showProgram(
+        ArrayList<Instruction> program
+    ) {
         programModel.clear();
 
         for (int i = 0; i < program.size(); i++) {
+
             String line = String.format(
                 "%04X  %s",
                 i,
@@ -49,38 +75,65 @@ public class ProgramPanel extends JPanel {
         int address,
         Instruction instruction
     ) {
+
         if (instruction == null) {
+
             currentInstructionLabel.setText(
                 "Current Instruction: None"
             );
+
+            categoryLabel.setText(
+                "Category: None"
+            );
+
             programList.clearSelection();
             return;
         }
 
         currentInstructionLabel.setText(
-            "Current Instruction: " + instruction.toString()
+            "Current Instruction: "
+            + instruction.toString()
         );
 
         programList.setSelectedIndex(address);
         programList.ensureIndexIsVisible(address);
     }
+
     public void showNextInstruction(
-    int address,
-    ArrayList<Instruction> program
-) {
-    if (address >= 0 && address < program.size()) {
-        currentInstructionLabel.setText(
-            "Next Instruction: " + program.get(address).toString()
-        );
+        int address,
+        ArrayList<Instruction> program
+    ) {
 
-        programList.setSelectedIndex(address);
-        programList.ensureIndexIsVisible(address);
-    } else {
-        currentInstructionLabel.setText(
-            "Next Instruction: None (Program Halted)"
-        );
+        if (address >= 0 && address < program.size()) {
 
-        programList.clearSelection();
+            currentInstructionLabel.setText(
+                "Next Instruction: "
+                + program.get(address).toString()
+            );
+
+            programList.setSelectedIndex(address);
+            programList.ensureIndexIsVisible(address);
+
+        } else {
+
+            currentInstructionLabel.setText(
+                "Next Instruction: None (Program Halted)"
+            );
+
+            categoryLabel.setText(
+                "Category: None"
+            );
+
+            programList.clearSelection();
+        }
     }
-}
+
+    public void showCategory(
+        String category
+    ) {
+
+        categoryLabel.setText(
+            "Category: " + category
+        );
+    }
 }
